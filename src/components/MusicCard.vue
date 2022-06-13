a2f036334a7d98063674f5f7a279060600dc1755
Claudio <claudiovarrosi@gmail.com>
2022-06-13 20:06:27 +0200

    Lavoro completo

--- src/components/MusicCard.vue
+++ src/components/MusicCard.vue
@@ -1,9 +1,12 @@
 <template>
+
+
   <div class="card">
     <img :src="musicObject.poster" :alt="musicObject.title">
-    <h2>{{musicObject.title}}</h2>
-    <p>{{musicObject.author}}</p>
-    <p>{{musicObject.year}}</p>
+    <h3>{{ musicObject.title }}</h3>
+    <p>{{ musicObject.author }}</p>
+    <p>{{ musicObject.year }}</p>
+    <p>{{ musicObject.genre }}</p>
   </div>
 </template>
 
@@ -18,22 +21,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    .card {
-        width: calc((100% / 5) - 20px);
-        background-color: #2e3a46;
-        height: auto;
-        margin: 10px;
-        text-align: center;
-        h2 {
-            color: white;
-        }
-        img {
-            width: 100%;
-            height: 80%;
-            object-fit: cover;
-        }
-        p {
-            color: lightslategray;
-        }
-    }
+.card {
+  width: calc((100% / 5) - 20px);
+  background-color: #2e3a46;
+  margin: 10px;
+  text-align: center;
+
+  h3 {
+    color: white;
+  }
+
+  img {
+    width: 100%;
+    height: 80%;
+    object-fit: cover;
+  }
+
+  p {
+    color: lightslategray;
+  }
+}
 </style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,28 +1,30 @@
 <template>
     <main>
+        <div>
+            <SelectBar @search="swapGenre"/>
+        </div>
         <div id="card_container">
-            <MusicCard 
-                v-for="(item, i) in musicCds"
-                :key="i"
-                :musicObject="item"
-            />
+            <MusicCard v-for="(item, i) in filteredCds" :key="i" :musicObject="item" />
         </div>
     </main>
 </template>
 
 <script>
+import SelectBar from './SelectBar.vue'
 import MusicCard from './MusicCard.vue'
 import axios from "axios"
 
 export default {
     name: "MyMain",
     components: {
-        MusicCard
+        MusicCard,
+        SelectBar
     },
     data() {
         return {
             apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
-            musicCds: []
+            musicCds: [],
+            activeGenre: "all"
         }
     },
     created() {
@@ -38,8 +40,23 @@ export default {
                 .catch((error) => {
                     console.log("Errore", error)
                 })
+        },
+        swapGenre(key) {
+            this.activeGenre=key
         }
     },
+    computed: {
+        filteredCds() {
+            if (this.activeGenre === "all") {
+                return this.musicCds;
+            } else {
+                return this.musicCds.filter(item => {
+                    return item.genre.toLowerCase().includes(this.activeGenre);
+                })
+            }
+        }
+    }
+
 }
 </script>
 
@@ -48,6 +65,10 @@ export default {
 main {
     background-color: #1e2d3b;
     padding: 30px 50px;
+
+    .label-search {
+        width: calc(100% / 5);
+    }
 
     #card_container {
         display: flex;
